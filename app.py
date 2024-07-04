658fa94b5870d960edbb0eb22dde612097d45f2d
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 22:30:46 +0100

    Fixed issue

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String)
+    content = db.Column(db.String(200), )
 
 @app.route("/")
 def index():
