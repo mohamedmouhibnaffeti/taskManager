78aa3fbf60a5f2d3b12bf2b104cda36c86b62a6f
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 22:31:20 +0100

    Fixed issue

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -8,7 +9,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime,)
+    date_created = db.Column(db.DateTime, default=datetime)
 
 @app.route("/")
 def index():
