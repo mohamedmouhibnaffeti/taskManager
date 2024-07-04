163c638b7237e6c1ef6ca8dae4b152a7dfb74d2b
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 22:28:39 +0100

    Fixed issue

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY']
+app.config['SQLALCHEMY_DATABASE_URI']
 
 @app.route("/")
 def index():
