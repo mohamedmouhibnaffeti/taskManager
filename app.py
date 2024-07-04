90c465f09a7abf20f81b02763f7892de63861fcc
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 23:23:47 +0100

    Fixed issue

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def deleteTask(id):
 
 @app.route("/update/<int:id>", methods=['GET', 'POST'])
 def updateTask(id):
-    return ''
+    if request
 
 def create_tables():
     with app.app_context():
