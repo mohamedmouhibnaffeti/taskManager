f1b62f0ac3e0a4ea75b5e775030578bb60561d92
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 22:29:05 +0100

    Fixed issue

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = 
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
