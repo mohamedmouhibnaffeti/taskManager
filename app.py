39992f17d0e3179cf1b4d787e0174e6168da608e
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-04 22:31:58 +0100

    Fixed issue

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    
+
+    def __rep__(self):
+        
 
 @app.route("/")
 def index():
